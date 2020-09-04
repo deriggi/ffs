87a9f130b1fa160a7bf81b26a2a4be81b5ff9ebb
John DeRiggi <john.deriggi@gmail.com>
2020-09-04 14:04:15 -0700

    refactors to remove duplicated code

--- src/main/java/sony/deriggi/ffs/controller/FfsController.java
+++ src/main/java/sony/deriggi/ffs/controller/FfsController.java
@@ -1,5 +1,7 @@
 package sony.deriggi.ffs.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import sony.deriggi.ffs.dto.ApiDatum;
 import sony.deriggi.ffs.dto.ApiMessage;
 import sony.deriggi.ffs.dto.FeatureStatus;
 import sony.deriggi.ffs.service.FfsService;
@@ -18,7 +21,7 @@ import sony.deriggi.ffs.service.FfsService;
 @RestController
 public class FfsController {
 
-
+    
     @Autowired
     FfsService ffsService;
 
@@ -30,11 +33,9 @@ public class FfsController {
 
     @CrossOrigin(origins = "http://localhost:1234" )
     @RequestMapping(value = "/ffs", method = RequestMethod.GET)
-    
 	public ResponseEntity<?> getSites() throws Exception {
         ApiMessage message  = ffsService.fetchData();
-        return ResponseEntity.status(message.getStatusCode())
-        .body(message.getDataItems());
+        return createResponse(message.getDataItems(), message.getStatusCode());
         
     }
 
@@ -42,8 +43,14 @@ public class FfsController {
     @RequestMapping(value = "/ffs", method = RequestMethod.POST)
 	public ResponseEntity<?> addOrUpdate(@RequestBody FeatureStatus fs) throws Exception {
         ApiMessage message  = ffsService.updateFeatures(fs);
-        return ResponseEntity.status(message.getStatusCode())
-        .body(message.getDataItems());
+        return createResponse(message.getDataItems(), message.getStatusCode());
+    }
+
+    private ResponseEntity<?>  createResponse(List<? extends ApiDatum> items, Integer statusCode){
+        return ResponseEntity.status(statusCode)
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(items);
+        
     }
     
 }
